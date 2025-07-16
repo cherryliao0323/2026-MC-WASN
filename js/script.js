7db98478cae50dfa9037fdf79bd698ce9a2fa9a1
cherryliao0323 <cherry01688@gmail.com>
2025-07-16 21:37:03 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,1 @@
-// smooth scroll for nav anchors
-document.querySelectorAll('.nav-link[href^="#"]').forEach(a=>{
-  a.addEventListener('click', e=>{
-    e.preventDefault();
-    document.querySelector(a.getAttribute('href'))
-      .scrollIntoView({behavior: 'smooth'});
-  });
-});
-console.log('MC & WASN site loaded.');
+console.log('MC & WASN 2025 website loaded.');
