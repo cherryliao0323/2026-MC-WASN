fad7fe48f1e4d3a3c6cfa60cab51cef8e6defc47
cherryliao0323 <cherry01688@gmail.com>
2025-07-17 10:03:19 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log('MC & WASN 2025 website loaded.');
+
+document.addEventListener("DOMContentLoaded", function () {
+  const links = document.querySelectorAll("a.nav-link[href^='#']");
+  for (const link of links) {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute("href"));
+      if (target) {
+        target.scrollIntoView({ behavior: "smooth" });
+      }
+    });
+  }
+});
